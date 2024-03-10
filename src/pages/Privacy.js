aa2f4e72995c407d01b71b9e603d05a4e9c4c252
Andrew Ferreira <andrewf289@gmail.com>
2024-03-09 20:44:28 -0500

    hd

---
+++ src/pages/Privacy.js
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+
+   
+
+const Privacy = () => {
+  
+
+  return (
+    <div className="container">
+        <h1>Privacy</h1>
+        
+        
+    </div>
+  );
+};
+
+export default Privacy;
