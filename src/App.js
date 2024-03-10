be34ad39b17c0b18518383db23e7e64c4f683968
Andrew Ferreira <andrewf289@gmail.com>
2024-03-09 20:50:37 -0500

    add priv testing

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import Creator from "./pages/Creator"
 import About from "./pages/About"
 import Contact from "./pages/Contact"
 import Blog from "./pages/Blog"
+import Privacy from "./pages/Privacy";
 
 import 
 {
@@ -37,6 +38,7 @@ function App() {
           <Route path="/about" element={<About/>}></Route>
           <Route path="/contact" element={<Contact/>}></Route>
           <Route path="/Blog" element={<Blog/>}></Route>
+          <Route path="/privacy" element={<Privacy/>}></Route>
 
           
         </Routes>
